c878b201f7bf77142825018f46576eba74563314
Andrew Samaha <asamaha@mountain.com>
2026-08-10 09:23:26 -0400

    fix: Unmaps netrw's left-click as enter behavior

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,9 +20,16 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "netrw",
 	callback = function(ev)
     vim.keymap.set("n", "%", touch_netrw_file, { buffer = ev.buf })
+
+    -- Netrw maps left-click to its file-browser handler.  Let Neovim handle
+    -- it normally so clicks and drags on the split separator can resize it.
+    vim.schedule(function()
+      if vim.api.nvim_buf_is_valid(ev.buf) and vim.bo[ev.buf].filetype == "netrw" then
+        pcall(vim.keymap.del, "n", "<LeftMouse>", { buffer = ev.buf })
+      end
+    end)
   end,
 })
 
 vim.keymap.set("n", "<leader>e", ":Lexplore<cr>", { silent = true })
-
 
